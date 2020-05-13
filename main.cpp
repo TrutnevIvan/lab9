c3cd6fbe048b6e767747925608a87b2b3ee17773
TrutnevIvan <www.vanya22@gmail.com>
2020-05-13 15:37:49 +0300

    Added sum
    Signed-off-by: TrutnevIvan <www.vanya22@gmail.com>

--- main.cpp
+++ main.cpp
@@ -2,13 +2,18 @@
 
 int main(){
 
-    int x1, x2;
+    int x1, x2, sum;
 
     printf("Input 1st integer number: ");
     scanf("%d", &x1);
 
     printf("\nInput 2nd integer number: ");
     scanf("%d", &x2);
+
+    printf("\nThe summa of 2 numbers = %d", x1+x2);
+
+    getchar();
+    getchar();
 
     return 0;
 }
